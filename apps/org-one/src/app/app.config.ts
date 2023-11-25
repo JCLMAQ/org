16322e680d74a231b090cb018ce70491d8cf9642
JCM <jcl.maquinay@gmail.com>
2023-11-25 11:47:19 +0100

    add minimale ngrx

--- apps/org-one/src/app/app.config.ts
+++ apps/org-one/src/app/app.config.ts
@@ -1,7 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideStoreDevtools({ logOnly: !isDevMode() }),
+    provideEffects(),
+    provideStore(),
+    provideRouter(appRoutes),
+  ],
 };
