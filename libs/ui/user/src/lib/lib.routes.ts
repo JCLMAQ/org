b7ad9f5d060c34f8eefd524d343f303f56b5815e
JCM <jcl.maquinay@gmail.com>
2023-11-25 12:09:47 +0100

    Add ngrx feature store

--- libs/ui/user/src/lib/lib.routes.ts
+++ libs/ui/user/src/lib/lib.routes.ts
@@ -1,6 +1,19 @@
 import { Route } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import * as fromUsers from './+state/users.reducer';
+import { UsersEffects } from './+state/users.effects';
+import { UsersFacade } from './+state/users.facade';
 
 export const userListRoutes: Route[] = [
-  { path: '', component: UserListComponent },
+  {
+    path: '',
+    component: UserListComponent,
+    providers: [
+      UsersFacade,
+      provideState(fromUsers.USERS_FEATURE_KEY, fromUsers.usersReducer),
+      provideEffects(UsersEffects),
+    ],
+  },
 ];
