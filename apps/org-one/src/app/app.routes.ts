0726b184e29059af1b1f95505531405d706a6cc6
JCM <jcl.maquinay@gmail.com>
2023-12-01 12:38:00 +0100

    add

--- apps/org-one/src/app/app.routes.ts
+++ apps/org-one/src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'task',
+    path: 'tasks',
     loadChildren: () => import('@org/task').then((m) => m.taskRoutes),
   },
   {
-    path: 'user-list',
+    path: 'users',
     loadChildren: () => import('@org/user-list').then((m) => m.userListRoutes),
   },
 ];
